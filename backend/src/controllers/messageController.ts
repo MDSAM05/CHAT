e6aea1c9ebcbec0843ada2c86d2db8fc212c5106
MDSAM05 <mhdsamr05@gmail.com>
2025-02-26 00:12:53 +0530

    con and mess

---
+++ backend/src/controllers/messageController.ts
@@ -0,0 +1,127 @@
+import { Request, Response } from "express"
+import prisma from "../db/prisma.js";
+
+
+export const sendMessage = async (req: Request, res: Response) =>{
+    try{
+        const { message } = req.body;
+        const { id: receiverId} = req.params;
+        const senderId = req.user.id;
+
+        let conversation = await prisma.conversation.findFirst({
+             where :{
+                    participantIDs : {
+                       hasEvery: [senderId,receiverId]
+                    }
+             } 
+        })
+
+        if(!conversation) {
+            conversation = await prisma.conversation.create({
+                data :
+                {
+                    participantIDs : {
+                         set: [senderId,receiverId]
+                     }
+                }
+            })
+        }
+        
+        const newmessage = await prisma.message.create({
+             data: {
+                senderId,
+                body: message,
+                conversationId: conversation.id
+             },
+
+        })
+
+        if(newmessage)
+        {
+            conversation = await prisma.conversation.update({
+                where :{
+                   id:  conversation.id
+                },
+                data : {
+                    messages : {
+                        connect : {
+                            id :newmessage.id
+                        }
+                    }
+                }
+            })
+        }
+        res.status(201).json(newmessage);
+
+    }
+    catch(error: any)
+    {
+        console.log("Error in sendMessage:", error.message);
+        res.status(500).json({ error :"Interal server error"})
+
+    }
+
+}
+
+export const getMessage = async (req: Request, res: Response): Promise<any> =>{
+  try {
+    const {id: chatid} = req.params;
+    const senderId = req.user.id;
+
+    const conversation = await prisma.conversation.findFirst({
+        where :{
+            participantIDs :{
+                hasEvery: [chatid, senderId]
+            }
+        },
+        include: {
+            messages: {
+               orderBy: {
+                createdAt: "asc"
+               }
+            }
+        }
+    })
+
+    if(!conversation)
+    {
+         res.status(200).json({messages: []})
+    }
+    if (conversation) {
+        return res.status(200).json(conversation.messages);
+    } else {
+        return res.status(404).json({ error: "Conversation not found" });
+    }
+  }
+  catch(error: any)
+  {
+    res.status(500).json({ error : "internal error"});
+    console.log("Error in getMessage: ", error.message);
+  }
+}
+
+export const getUsersforSidebar = async (req: Request, res: Response): Promise<any>  => {
+   try{
+    const authUser = req.user.id;
+
+    const users = await prisma.user.findMany({
+        where :
+        {
+            id :{
+                not:authUser
+            }
+        },
+        select: {
+            id: true,
+            username: true,
+            profile: true
+        }
+    }) 
+    return res.status(200).json(users);
+   }
+   catch(error: any)
+   {
+       console.log("Error in sendMessage:", error.message);
+       res.status(500).json({ error :"Interal server error"})
+   }
+}
--- backend/src/router/messagerouter.ts
+++ backend/src/router/messagerouter.ts
@@ -1,5 +1,10 @@
 import express from "express";
-
+import protectRoute from "../middleware/protectRoute.js";
+import { sendMessage, getMessage,getUsersforSidebar } from "../controllers/messageController.js"
 const router = express.Router();
-        
+
+router.post("/send/:id", protectRoute, sendMessage);
+router.get("/conversations", protectRoute, getUsersforSidebar)
+router.get("/:id", protectRoute, getMessage);
+
 export default router;
